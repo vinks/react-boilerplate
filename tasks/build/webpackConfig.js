0e33fc0cd637e35ba018b639a10f555a2906ea90
Konstantin Vinogradov <vin.konst@gmail.com>
2017-05-31 17:55:08 +0300

    use env port

--- tasks/build/webpackConfig.js
+++ tasks/build/webpackConfig.js
@@ -13,8 +13,8 @@ module.exports = [
     // has landed a better fix for relative image references from
     // sourcemapped css.
     // https://github.com/webpack/style-loader#recommended-configuration
-    // https://github.com/webpack/style-loader/issues/55
-    publicPath: 'http://localhost:6060/'
+    // https://github.com/wprocess.env.NODE_ENVebpack/style-loader/issues/55
+    publicPath: `http://localhost:${process.env.PORT}/`
   }),
   createConfig({
     name: 'server',
